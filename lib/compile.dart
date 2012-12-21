9a4f5b669d9af83b703e97bc83653d7fe5f0082a
simonpai <simonpai@potix.com>
2012-12-21 15:53:44 +0800

    Add auto-compile function for working with build.dart.

--- lib/compile.dart
+++ lib/compile.dart
@@ -5,15 +5,75 @@
 library rikulo_uxl_compile;
 
 import 'dart:io';
+import 'package:args/args.dart';
 import 'package:html5plus/dom.dart';
+import 'package:html5plus/parser.dart' show HtmlParser;
 
 part "src/compiler/Compiler.dart";
 part "src/compiler/Util.dart";
 
-/** Compiles the given UXL document to the given output stream.
+/** Compiles the given [source] UXL document to the given output stream [out].
  * Notice that the caller has to close the output stream by himself.
  */
 void compile(Document source, OutputStream out, {
 String sourceName, Encoding encoding: Encoding.UTF_8, bool verbose: false}) {
   new Compiler(source, out, sourceName: sourceName, encoding: encoding, verbose: verbose).compile();
+}
+
+/** Compiles the UXL document of the given [sourceName] and write the result to
+ * the file of given [destinationName].
+ */
+void compileFile(String sourceName, {String destinationName, bool verbose : false, 
+Encoding encoding}) {
+  
+  final source = new File(sourceName);
+  if (!source.existsSync()) {
+    print("File not found: ${sourceName}");
+    return;
+  }
+  
+  if (destinationName == null) {
+    final int i = sourceName.lastIndexOf('.');
+    final int j = sourceName.lastIndexOf('/');
+    destinationName = i >= 0 && j < i ? "${sourceName.substring(0, i + 1)}dart" : "${sourceName}.dart";
+  }
+  final dest = new File(destinationName);
+  
+  if (verbose) {
+    final int i = dest.name.lastIndexOf('/') + 1;
+    print("Compile ${source.name} to ${i > 0 ? dest.name.substring(i) : dest.name}");
+  }
+  
+  source.readAsString(encoding).then((text) {
+    final out = dest.openOutputStream();
+    try {
+      compile(
+          new HtmlParser(text, encoding: encoding.name, lowercaseElementName: false, 
+              lowercaseAttrName: false, cdataOK: true).parseFragment(),
+          out, sourceName: sourceName, encoding: encoding, verbose: verbose);
+    } finally {
+      out.close();
+    }
+  });
+  
+}
+
+/** Compile changed UXL files. This method shall be called within build.dart,
+ * with new Options().arguments as its [arguments].
+ */
+void build(List<String> arguments) {
+  final ArgParser argParser = new ArgParser()
+    ..addOption("changed", allowMultiple: true)
+    ..addOption("removed", allowMultiple: true)
+    ..addFlag("clean", negatable: false);
+  
+  final ArgResults args = argParser.parse(arguments);
+  final List<String> changed = args["changed"];
+  final List<String> removed = args["removed"];
+  final bool clean = args["clean"];
+  
+  for (String name in changed)
+    if (name.endsWith(".uxl.xml"))
+      compileFile(name);
+  
 }
